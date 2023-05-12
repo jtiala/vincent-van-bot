543d1f19d670013112f2a93377fd39971de83e23
Joonas Tiala <joonas.tiala@gmail.com>
2023-05-12 19:52:26 +0300

    Add point validator

--- src/move.ts
+++ src/move.ts
@@ -5,38 +5,23 @@ import { Bot } from "./types/Bot";
 import { DiagonalDir, Dir } from "./types/Dirs";
 import { Point } from "./types/Point";
 
-export const moveBot = async (
-  bot: Bot,
-  dir: Dir | DiagonalDir,
-  dist: number
-): Promise<Bot> => {
-  if (isDiagonalDir(dir)) {
-    const dirs = diagonalDirToDirs(dir);
-
-    for (const dir of dirs) {
-      for (let i = 0; i < dist; i++) {
-        bot = await api.moveBot(bot, dir);
-      }
-    }
-
-    return bot;
-  }
-
-  for (let i = 0; i < dist; i++) {
-    bot = await api.moveBot(bot, dir);
-  }
-
-  return bot;
-};
-
-export const goto = async (bot: Bot, position: Point): Promise<Bot> => {
-  const { x, y } = position;
+export const isValidPoint = (point: Point) => {
+  const { x, y } = point;
 
   if (x < 1 || y < 1 || x > canvasWidth || y > canvasHeight) {
     console.log(`Invalid point: ${x},${y}`);
+    return false;
+  }
 
+  return true;
+};
+
+export const goto = async (bot: Bot, position: Point): Promise<Bot> => {
+  if (!isValidPoint(position)) {
     return bot;
   }
+
+  const { x, y } = position;
 
   if (bot.position?.x && bot.position.x < x) {
     for (let i = bot.position.x; i < x; i++) {
@@ -60,6 +45,30 @@ export const goto = async (bot: Bot, position: Point): Promise<Bot> => {
     for (let i = bot.position.y; i > y; i--) {
       bot = await api.moveBot(bot, "UP");
     }
+  }
+
+  return bot;
+};
+
+export const moveBot = async (
+  bot: Bot,
+  dir: Dir | DiagonalDir,
+  dist: number
+): Promise<Bot> => {
+  if (isDiagonalDir(dir)) {
+    const dirs = diagonalDirToDirs(dir);
+
+    for (const dir of dirs) {
+      for (let i = 0; i < dist; i++) {
+        bot = await api.moveBot(bot, dir);
+      }
+    }
+
+    return bot;
+  }
+
+  for (let i = 0; i < dist; i++) {
+    bot = await api.moveBot(bot, dir);
   }
 
   return bot;
