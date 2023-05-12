221ca23bcbffbea53a238262f51721c1a9ead8a0
Joonas Tiala <joonas.tiala@gmail.com>
2023-05-12 14:24:08 +0300

    Move drawing to own file

---
+++ src/draw.ts
@@ -0,0 +1,29 @@
+import * as api from "./Api";
+import { Bot } from "./types/Bot";
+import { Dir, dirs } from "./types/Dirs";
+import { moveBotDiagonal } from "./util";
+
+export const drawRectangle = async (bot: Bot, width: number): Promise<Bot> => {
+  for (const dir of dirs) {
+    for (let i = 0; i < width; i++) {
+      await api.moveBot(bot, dir);
+      await api.paintPixel(bot);
+    }
+  }
+
+  return bot;
+};
+
+export const drawLine = async (
+  bot: Bot,
+  dir: Dir,
+  width: number
+): Promise<Bot> => {
+  for (let i = 1; i < width; i++) {
+    await api.paintPixel(bot);
+    await api.moveBot(bot, dir);
+  }
+
+  return bot;
+};
+
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import * as api from "./Api";
-import { Bot } from "./types/Bot";
-import { moveBot, registerBot } from "./util";
+import { registerBot } from "./util";
 
 const botName = "Vincent van Bot";
-
-// See color palette documentation in api.setColor
 const botColor = 9;
 
 const sayings = [
@@ -21,56 +18,18 @@ const sayings = [
   "There is no blue without yellow and without orange.",
 ];
 
-/**
- * Example helper functions for drawing a simple rectangle using the api calls
- * Here we are moving the bot first to a certain direction and then painting a
- * pixel with a color that was previously set in the main function.
- * @param bot
- * @param width
- */
-const drawRectangle = async (bot: Bot, width: number): Promise<Bot> => {
-  const dirs = ["RIGHT", "DOWN", "LEFT", "UP"];
-
-  for (const dir of dirs) {
-    for (let i = 0; i < width; i++) {
-      await api.moveBot(bot, dir);
-      await api.paintPixel(bot);
-    }
-  }
-
-  return bot;
-};
-
 export async function main() {
   let bot = await registerBot(botName);
   bot = await api.setColor(bot, botColor);
   bot = await api.say(bot, sayings[Math.floor(Math.random() * sayings.length)]);
 
-  // Draw some simple rectangles for example (make your own helper functions for more complex shapes!)
-  bot = await drawRectangle(bot, 6);
-  bot = await moveBot(bot, "RIGHT", 4);
-  bot = await drawRectangle(bot, 2);
-  bot = await moveBot(bot, "RIGHT", 6);
-  bot = await drawRectangle(bot, 6);
-  bot = await moveBot(bot, "RIGHT", 4);
-  bot = await drawRectangle(bot, 2);
-  bot = await moveBot(bot, "RIGHT", 8);
-
   console.log(
     `Current bot position: ${bot.position?.x},${bot.position?.y} and current bot color: ${bot.color}`
   );
 
-  // Print the current state of canvas in ASCII
-  // console.log(await api.look(bot))
+  console.log(await api.bots(bot));
 
-  // Get the current state of all registered bots (json)
-  // Useful i.e. for bots that want to utilize some swarming behaviour
-  // console.log(await api.bots(bot))
-
-  // Call 'deregisterBot' if you want to remove your bot from the server and, for example, change your bot name.
-  // Your bot key is stored in botConfig.cfg after registration, and it is reused when you run this script again.
-  // The deregister_bot command will remove the botConfig.cfg file automatically.
-  // await deregisterBot(bot)
+  // await deregisterBot(bot);
 }
 
 if (require.main === module) {
