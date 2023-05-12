c5aeb4b719dbca8db9b9be099e7fa2b13848715e
Joonas Tiala <joonas.tiala@gmail.com>
2023-05-12 19:27:25 +0300

    Combine drawDiagonalLine and drawLine

--- src/draw.ts
+++ src/draw.ts
@@ -16,39 +16,40 @@ export const drawRectangle = async (bot: Bot, width: number): Promise<Bot> => {
 
 export const drawLine = async (
   bot: Bot,
-  dir: Dir,
+  dir: Dir | DiagonalDir,
   length: number
 ): Promise<Bot> => {
+  if (
+    dir === "RIGHT_UP" ||
+    dir === "RIGHT_DOWN" ||
+    dir === "LEFT_UP" ||
+    dir === "LEFT_DOWN"
+  ) {
+    const dirs: Dir[] =
+      dir === "RIGHT_DOWN"
+        ? ["RIGHT", "DOWN"]
+        : dir === "RIGHT_UP"
+        ? ["RIGHT", "UP"]
+        : dir === "LEFT_DOWN"
+        ? ["LEFT", "DOWN"]
+        : ["LEFT", "UP"];
+
+    for (let i = 1; i < length; i++) {
+      bot = await api.paintPixel(bot);
+
+      for (const dir of dirs) {
+        bot = await api.moveBot(bot, dir);
+      }
+    }
+
+    bot = await api.paintPixel(bot);
+
+    return bot;
+  }
+
   for (let i = 1; i < length; i++) {
     bot = await api.paintPixel(bot);
     bot = await moveBot(bot, dir, 1);
-  }
-
-  bot = await api.paintPixel(bot);
-
-  return bot;
-};
-
-export const drawDiagonalLine = async (
-  bot: Bot,
-  dir: DiagonalDir,
-  length: number
-): Promise<Bot> => {
-  const dirs: Dir[] =
-    dir === "RIGHT_DOWN"
-      ? ["RIGHT", "DOWN"]
-      : dir === "RIGHT_UP"
-      ? ["RIGHT", "UP"]
-      : dir === "LEFT_DOWN"
-      ? ["LEFT", "DOWN"]
-      : ["LEFT", "UP"];
-
-  for (let i = 1; i < length; i++) {
-    bot = await api.paintPixel(bot);
-
-    for (const dir of dirs) {
-      bot = await api.moveBot(bot, dir);
-    }
   }
 
   bot = await api.paintPixel(bot);
@@ -62,79 +63,79 @@ export const dickbutHeight = 26;
 export const drawDickbutt = async (bot: Bot): Promise<Bot> => {
   bot = await moveBot(bot, "RIGHT", 4);
   bot = await drawLine(bot, "RIGHT", 5);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 4);
+  bot = await drawLine(bot, "RIGHT_DOWN", 4);
   bot = await drawLine(bot, "DOWN", 11);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "RIGHT", 3);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 2);
+  bot = await drawLine(bot, "RIGHT_DOWN", 2);
   bot = await drawLine(bot, "DOWN", 2);
   bot = await drawLine(bot, "LEFT", 3);
   bot = await drawLine(bot, "RIGHT", 3);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 5);
+  bot = await drawLine(bot, "RIGHT_UP", 5);
   bot = await drawLine(bot, "RIGHT", 2);
   bot = await drawLine(bot, "DOWN", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 3);
+  bot = await drawLine(bot, "LEFT_DOWN", 3);
   bot = await drawLine(bot, "RIGHT", 2);
   bot = await drawLine(bot, "DOWN", 2);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_DOWN", 2);
   bot = await drawLine(bot, "LEFT", 2);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_DOWN", 2);
   bot = await drawLine(bot, "DOWN", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 3);
+  bot = await drawLine(bot, "LEFT_DOWN", 3);
   bot = await drawLine(bot, "LEFT", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_DOWN", 2);
   bot = await drawLine(bot, "DOWN", 4);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_DOWN", 2);
   bot = await drawLine(bot, "LEFT", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "RIGHT", 2);
   bot = await drawLine(bot, "UP", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
   bot = await drawLine(bot, "LEFT", 4);
   bot = await drawLine(bot, "DOWN", 5);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_DOWN", 2);
   bot = await drawLine(bot, "LEFT", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "RIGHT", 2);
   bot = await drawLine(bot, "UP", 4);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
   bot = await drawLine(bot, "UP", 2);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
   bot = await drawLine(bot, "UP", 2);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 2);
+  bot = await drawLine(bot, "LEFT_UP", 2);
   bot = await drawLine(bot, "UP", 6);
   bot = await drawLine(bot, "RIGHT", 7);
   bot = await drawLine(bot, "LEFT", 7);
   bot = await drawLine(bot, "UP", 4);
   bot = await drawLine(bot, "RIGHT", 3);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 2);
+  bot = await drawLine(bot, "RIGHT_DOWN", 2);
   bot = await drawLine(bot, "LEFT", 2);
   bot = await drawLine(bot, "UP", 3);
   bot = await drawLine(bot, "RIGHT", 2);
-  bot = await drawDiagonalLine(bot, "LEFT_UP", 4);
+  bot = await drawLine(bot, "LEFT_UP", 4);
   bot = await drawLine(bot, "DOWN", 4);
   bot = await drawLine(bot, "LEFT", 2);
   bot = await drawLine(bot, "UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 4);
+  bot = await drawLine(bot, "RIGHT_UP", 4);
   bot = await moveBotDiagonal(bot, "RIGHT_DOWN", 4);
   bot = await drawLine(bot, "RIGHT", 2);
   bot = await drawLine(bot, "LEFT", 3);
-  bot = await drawDiagonalLine(bot, "LEFT_DOWN", 2);
+  bot = await drawLine(bot, "LEFT_DOWN", 2);
   bot = await drawRectangle(bot, 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 2);
+  bot = await drawLine(bot, "RIGHT_DOWN", 2);
   bot = await drawLine(bot, "DOWN", 2);
   bot = await drawLine(bot, "RIGHT", 3);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "UP", 2);
   bot = await moveBot(bot, "DOWN", 7);
   bot = await moveBot(bot, "LEFT", 1);
   bot = await drawLine(bot, "DOWN", 3);
-  bot = await drawDiagonalLine(bot, "RIGHT_DOWN", 2);
+  bot = await drawLine(bot, "RIGHT_DOWN", 2);
   bot = await drawLine(bot, "DOWN", 2);
   bot = await drawLine(bot, "LEFT", 2);
   bot = await drawLine(bot, "DOWN", 2);
@@ -142,7 +143,7 @@ export const drawDickbutt = async (bot: Bot): Promise<Bot> => {
   bot = await drawLine(bot, "UP", 2);
   bot = await drawLine(bot, "LEFT", 2);
   bot = await drawLine(bot, "UP", 2);
-  bot = await drawDiagonalLine(bot, "RIGHT_UP", 2);
+  bot = await drawLine(bot, "RIGHT_UP", 2);
   bot = await drawLine(bot, "UP", 3);
   bot = await moveBotDiagonal(bot, "RIGHT_DOWN", 13);
 
